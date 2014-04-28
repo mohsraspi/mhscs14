e455fb4e9f1f60bdfce6e07f12eacf2780236b61
Jay <jay@classraspi.com>
2014-04-28 13:26:01 +0000

    Jay

---
+++ jay/killime.py
@@ -0,0 +1,14 @@
+import minecraft as minecraft
+import time
+
+
+mc = minecraft.Minecraft.create()
+friend = minecraft.Minecraft.create("10.52.3.16")
+
+while True == True:
+    friend.setBlock(0,-4,0,46,1)
+    time.sleep(2)
+
+
+
+
